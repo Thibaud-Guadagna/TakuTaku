69cc288434b441a1acb34db80cdc8f81cd2c1d6a
thibaud-guadagna <thibaud.guadagna@gmail.com>
2025-07-21 16:42:54 +0200

    fix2

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,9 @@
 // Load the express module to create a web application
 
 import express from "express";
-
+import cors from "cors";
 const app = express();
+
 
 // Configure it
 
@@ -18,7 +19,6 @@ const app = express();
 // You should NOT do that: such code uses the `cors` module to allow all origins, which can pose security issues.
 // For this pedagogical template, the CORS code allows CLIENT_URL in development mode (when process.env.CLIENT_URL is defined).
 
-import cors from "cors";
 
 if (process.env.CLIENT_URL != null) {
   app.use(cors({ origin: [process.env.CLIENT_URL] }));
